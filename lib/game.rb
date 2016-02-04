d009b75cf55530423e54112eea323813ec454ded
yiorgoss <makridakisyiorgos@gmail.com>
2016-02-04 18:32:37 +0000

    added the turn switch method and linked to the plaplayer number.

--- lib/game.rb
+++ lib/game.rb
@@ -1,15 +1,16 @@
 class Game
 
-  attr_reader
+  attr_reader :turn_count
 
   def initialize(p1, p2)
     @players = [p1, p2]
+    @turn_count = 1
   end
 
-  def player(number)
+  def player(number=(turn_count + 1))
     message = 'Please specify a valid player number'
     raise message if !number.is_a?(Numeric)
-    raise message if (number > players.size || number <= 0 )
+    raise message if (number > players.size || number < 0 )
     players[number - 1]
   end
 
@@ -17,8 +18,13 @@ class Game
     opponent.receive_damage
   end
 
-  # current_player
+  def next_turn
+    @turn_count += 1
+    @turn_count %= 2
+  end
 
   private
+
   attr_reader :players
+
 end
